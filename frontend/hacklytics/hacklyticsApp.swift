95e63ef8a83db8c5d8b3f4a843373da9d8cbf7a4
Nathan Bai <nathanbai50@gmail.com>
2026-02-22 04:24:07 -0500

    moved to frontend folder

---
+++ frontend/hacklytics/hacklyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hacklyticsApp.swift
+//  hacklytics
+//
+//  Created by Nathan Bai on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct hacklyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
